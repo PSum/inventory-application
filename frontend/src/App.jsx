5be72e84ab275a7e17817e5863c385386cbf68d6
psum <sum.philipp@gmail.com>
2024-09-09 16:54:13 +0200

    update
    added fetching of data from database

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,30 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
+import axios from 'axios';
 
 function App() {
   const [count, setCount] = useState(0)
+  const [catalog, setCatalog] = useState([]);
+
+function Catalog () {
+  const fetchCatalog = async () => {
+  try {
+    const response = await axios.get('http://localhost:3000');
+    setCatalog(response.data);
+    console.log(response.data);
+  } catch (error){
+    console.log("Error fetching data", error);
+  }
+  }
+  useEffect(()=>{
+    fetchCatalog();
+  },[])
+}
+
 
   return (
     <>
-    <div>Test</div>
+    <Catalog></Catalog>
     </>
   )
 }
